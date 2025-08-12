48ac568d5a30f9bc830507ad39b599a8968f5dba
Daniel Guerrero <dgmz@users.noreply.github.com>
2025-08-12 13:33:47 +0200

    Add tool to create DMG file.

---
+++ make-dmg.sh
@@ -0,0 +1,7 @@
+create-dmg \
+    --volname "Servo Tool" \
+    --volicon icons/feetech-tool.icns \
+    --icon "Servo Tool.app" 175 120 \
+    --app-drop-link 425 120 \
+    "dist/Servo Tool.dmg" \
+    dist/dmg
