f97bbc14bb1cc518255bb85debf8b876a4c8afc9
Daniel Guerrero <dgmz@users.noreply.github.com>
2025-08-25 16:52:41 +0200

    Build single file on Linux.

--- build-linux.sh
+++ build-linux.sh
@@ -1,1 +1,1 @@
-pyinstaller --name 'Servo Tool' --icon 'icons/feetech-tool.png' --windowed main.py
+pyinstaller --name 'Servo Tool' --icon 'icons/feetech-tool.png' --add-data icons/feetech-tool.png:icons/ --windowed --onefile main.py
