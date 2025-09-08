2aca993d7584441134a1202a989540e62a5c0e78
Daniel Guerrero <dgmz@users.noreply.github.com>
2025-09-08 18:08:52 +0200

    Add commands to generate AppImage.

--- build-linux.sh
+++ build-linux.sh
@@ -1,1 +1,9 @@
-pyinstaller --name 'Servo Tool' --icon 'icons/feetech-tool.png' --add-data icons/feetech-tool.png:icons/ --windowed --onefile main.py
+pyinstaller --name 'Servo-Tool' --icon 'icons/feetech-tool.png' --add-data icons/feetech-tool.png:icons/ --windowed --onefile main.py
+mkdir -p dist/Servo-Tool.AppDir/usr/bin
+cp -f dist/Servo-Tool dist/Servo-Tool.AppDir/usr/bin
+ln -sf dist/Servo-Tool.AppDir/usr/bin dist/AppRun
+cp -f Servo-Tool.desktop dist/Servo-Tool.AppDir/
+cp -f icons/feetech-tool.png dist/Servo-Tool.AppDir/Servo-Tool.png
+cd dist
+appimagetool Servo-Tool.AppDir/
+
