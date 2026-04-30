7281bc5435ef38a746c8fab8b8eba54e23db2d5e
Daniel <dgmz@users.noreply.github.com>
2026-04-30 19:36:31 +0200

    Fix dmg creation.

--- make-dmg.sh
+++ make-dmg.sh
@@ -1,3 +1,5 @@
+mkdir dist/dmg/
+cp -r dist/Servo\ Tool.app dist/dmg/
 create-dmg \
     --volname "Servo Tool" \
     --volicon icons/feetech-tool.icns \
@@ -5,3 +7,4 @@ create-dmg \
     --app-drop-link 425 120 \
     "dist/Servo Tool.dmg" \
     dist/dmg
+rm -r dist/dmg/
